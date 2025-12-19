ea6731d9c37725600e5aea9dfbff8e21504d6096
hugobovareto <hugobovareto@gmail.com>
2025-12-19 11:13:41 -0300

    Atualização dos dados para 18/12/2025

--- Página_Inicial.py
+++ Página_Inicial.py
@@ -145,7 +145,7 @@ st.markdown("""
 
 st.write("")
 st.markdown("""
-**⏱️ Última atualização**:  dados extraídos do SIGEduc em 11/12/2025.
+**⏱️ Última atualização**:  dados extraídos do SIGEduc em 18/12/2025.
 """)
 
 st.write("")
